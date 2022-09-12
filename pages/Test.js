a4ce5c200528c788dd908c7f723f37609847542d
Nguyen Duy Quang <nguyenduyquang2002@gmail.com>
2022-09-12 23:50:34 +0700

    Global styling
    Add global styling (header, body, small, details) + Make page test (just for general global styling, not variables yet)...

---
+++ pages/Test.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Test() {
+  return (
+    <div>
+      <h1>Đây là h1</h1>
+      <h2>Đây là h2</h2>
+      <h3>Đây là h3</h3>
+      <h4>Đây là h4</h4>
+      <h5>Đây là h5</h5>
+      <body>
+        <p>Đây là body.</p>
+        <small>Đây là small (hoặc smaller)</small>
+        <details>Đây là details</details>
+      </body>
+    </div>
+  )
+}

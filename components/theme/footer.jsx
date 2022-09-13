3e3203e9340963c5d875ebc4305fe9647f27b2fb
Nguyen Duy Quang <nguyenduyquang2002@gmail.com>
2022-09-14 00:05:27 +0700

    Position + title
    Mostly working on component's position + basic title (?)...

--- components/theme/footer.jsx
+++ components/theme/footer.jsx
@@ -6,23 +6,24 @@ export default function Footer() {
   return (
     <div className={styles.footer}>
       {/* Title */}
-      <div> 
+      <h5 className={styles.title}> 
         Vườn hoa sứ
-      </div>
+      </h5>
       
       {/* Icon */}
       <div>
-
+        
       </div>
 
       {/* Long text */}
       <div>
-
+        Dự án Vườn Hoa Sứ là sản phẩm của đội ngũ MEO Studio. Mọi nhân vật, sự kiện, địa điểm được tác phẩm nhắc đến đều là giả tưởng.<br/>
+        Vui lòng tôn trọng bản quyền.
       </div>
 
       {/* Short text */}
       <div>
-
+        Trở thành thế hệ tiên phong với MEO Studio - Initiate
       </div>
     </div>
   )

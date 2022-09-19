73c0ff2339d45767a84b6144cb44fb1a5358def4
Nguyen Duy Quang <nguyenduyquang2002@gmail.com>
2022-09-20 00:15:02 +0700

    Adjusting with global styling
    Adjust small thing to make it compatible with new global styling...

--- components/theme/footer.jsx
+++ components/theme/footer.jsx
@@ -8,28 +8,30 @@ export default function Footer() {
   return (
     <div className={styles.footer}>
       {/* Title */}
-      <h5 className={styles.title}> 
-        Vườn Hoa Sứ
-      </h5>
+      <div className={styles.title}> 
+        <div className='sub-header-4'>Vườn Hoa Sứ</div>
+      </div>
       
       {/* Icon */}
       <div className={styles.iconContainer}>
         <FaFacebook className={styles.icon}/>
         <FaYoutube className={styles.icon}/>
-        {/* <div className={styles.icon}><Image src='/icons/facebook.svg' alt='facebook' layout='fill' objectFit='contain'/></div>
-        <div className={styles.icon}><Image src='/icons/youtube.svg' alt='facebook' layout='fill' objectFit='contain'/></div> */}
       </div>
 
       {/* Long text */}
-      <small className={styles.mainText}>
-        Dự án Vườn Hoa Sứ là sản phẩm của đội ngũ MEO Studio. Mọi nhân vật, sự kiện, địa điểm được tác phẩm nhắc đến đều là giả tưởng.<br/>
-        Vui lòng tôn trọng bản quyền.
-      </small>
+      <div className={styles.mainText}>
+        <div className='details'>
+          Dự án Vườn Hoa Sứ là sản phẩm của đội ngũ MEO Studio. Mọi nhân vật, sự kiện, địa điểm được tác phẩm nhắc đến đều là giả tưởng.<br/>
+          Vui lòng tôn trọng bản quyền.
+        </div>
+      </div>
 
       {/* Short text */}
-      <small className={styles.subText}>
-        Trở thành thế hệ tiên phong với MEO Studio - Initiate
-      </small>
+      <div className={styles.subText}>
+        <div className='details'>
+          Trở thành thế hệ tiên phong với MEO Studio - Initiate
+        </div>
+      </div>
     </div>
   )
 }

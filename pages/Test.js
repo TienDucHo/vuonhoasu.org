c601edfbfb55534acccbfc793637ec68d114d31e
Nguyen Duy Quang <nguyenduyquang2002@gmail.com>
2022-09-20 18:05:32 +0700

    Rename camelUpperCase (1)
    Rename class + minor edit for test page...

--- pages/Test.js
+++ pages/Test.js
@@ -4,10 +4,10 @@ export default function Test() {
   return (
     <div>
       <h1 className='header'>Đây là h1</h1>
-      <h2 className='sub-header'>Đây là h2</h2>
-      <h3 className='sub-header-2'>Đây là h3</h3>
-      <h4 className='sub-header-3'>Đây là h4</h4>
-      <h5 className='sub-header-4'>Đây là h5</h5>
+      <h2 className='subHeader'>Đây là h2</h2>
+      <h3 className='subHeader2'>Đây là h3</h3>
+      <h4 className='subHeader3'>Đây là h4</h4>
+      <h5 className='subHeader4'>Đây là h5</h5>
       <p className='body'>Đây là body.</p>
       <p className='smallest'>Đây là smallest.</p>
       <p className='details'>Đây là details.</p>

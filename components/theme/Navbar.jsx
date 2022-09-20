90486077c37341110ed7bd3218bbd4ac07533d9a
Nguyen Duy Quang <nguyenduyquang2002@gmail.com>
2022-09-20 18:18:26 +0700

    Change name class
    Adjust class name to the new global styling...

--- components/theme/Navbar.jsx
+++ components/theme/Navbar.jsx
@@ -38,23 +38,23 @@ export default function Navbar() {
           clicked && 
           <div className={styles.navBarPhone} ref={dropdown}>
             <div className={styles.title}>
-              <div className='sub-header-2'>Vườn Hoa Sứ</div>
+              <div className='subHeader2'>Vườn Hoa Sứ</div>
             </div>
             {menuSection.map(section => 
             <div className={styles.item} key={section.id}>
-              <div className='sub-header-4'>{section.context}</div>
+              <div className='subHeader4'>{section.context}</div>
             </div>)}
           </div>
         }
         {/* Left section aka logo (Page's name, does it count as logo?)*/}
         <div className={styles.left}>
-          <div className='sub-header-2'>Vườn Hoa Sứ</div>
+          <div className='subHeader2'>Vườn Hoa Sứ</div>
         </div>
         {/* Right section */}
         <div className={styles.right}>
             {menuSection.map(section => 
             <div className={styles.rightItem} key={section.id}>
-              <div className='sub-header-4'>{section.context}</div>
+              <div className='subHeader4'>{section.context}</div>
             </div>)}
             <button type='button' className={styles.rightItemPhone} onClick={e => handleClick(e)}>
               <FaBars size='32px' color='#fbc846'/>

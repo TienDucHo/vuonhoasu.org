80e83a8a00a99204e27d9b4d182843eeaa9353c0
Duc Ho <hotienduc05012002@gmail.com>
2022-09-24 00:03:29 -0600

    Rename component
    For f sake Quang

--- components/theme/footer.jsx
+++ components/theme/footer.jsx
@@ -1,29 +1,40 @@
-import React from 'react'
+import React from "react";
 
-import styles from './footer.module.scss'
+import { FaFacebook, FaYoutube } from "react-icons/fa";
+
+import styles from "./Footer.module.scss";
 
 export default function Footer() {
   return (
     <div className={styles.footer}>
       {/* Title */}
-      <div> 
-        Vườn hoa sứ
+      <div className={styles.title}>
+        <div className="subHeader4">Vườn Hoa Sứ</div>
       </div>
-      
-      {/* Icon */}
-      <div>
 
+      {/* Icon */}
+      <div className={styles.iconContainer}>
+        <FaFacebook className={styles.icon} />
+        <FaYoutube className={styles.icon} />
       </div>
 
       {/* Long text */}
-      <div>
-
+      <div className={styles.mainText}>
+        <div className="details">
+          Dự án Vườn Hoa Sứ là sản phẩm của đội ngũ MEO Studio. Mọi
+          nhân vật, sự kiện, địa điểm được tác phẩm nhắc đến đều là
+          giả tưởng.
+          <br />
+          Vui lòng tôn trọng bản quyền.
+        </div>
       </div>
 
       {/* Short text */}
-      <div>
-
+      <div className={styles.subText}>
+        <div className="details">
+          Trở thành thế hệ tiên phong với MEO Studio - Initiate
+        </div>
       </div>
     </div>
-  )
+  );
 }

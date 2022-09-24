10497227beba05d88ef143d914f9327a51d96a87
Duc Ho <hotienduc05012002@gmail.com>
2022-09-23 23:58:46 -0600

    Rebasing

---
+++ components/theme/footer.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import styles from './footer.module.scss'
+
+export default function Footer() {
+  return (
+    <div className={styles.footer}>
+      {/* Title */}
+      <div> 
+        Vườn hoa sứ
+      </div>
+      
+      {/* Icon */}
+      <div>
+
+      </div>
+
+      {/* Long text */}
+      <div>
+
+      </div>
+
+      {/* Short text */}
+      <div>
+
+      </div>
+    </div>
+  )
+}

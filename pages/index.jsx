94326e419b2db1ce933be52921776dbaf334b46a
Duc Ho <hotienduc05012002@gmail.com>
2022-10-01 07:26:39 -0600

    Fix dependencies

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,12 @@
 import Head from "next/head";
-import Image from "next/Image";
+import Image from "next/image";
 import styles from "../styles/Home.module.scss";
 import profilePic from "../public/pictures/meo.jpg";
-import profilePic2 from "../public/pictures/background.jpg"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import profilePic2 from "../public/pictures/background.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFire } from "@fortawesome/free-solid-svg-icons";
 import { faStar } from "@fortawesome/free-regular-svg-icons";
-import { COLORS } from "../public/constants.jsx"
+import { COLORS } from "../public/constants.jsx";
 
 export default function Home() {
   return (
@@ -23,31 +23,64 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.gridContainer1}>
           <div className={styles.profilePic}>
-            <Image className={styles.profilePicStyling} src={profilePic} width={600} height={600}  />
+            <Image
+              className={styles.profilePicStyling}
+              src={profilePic}
+              width={600}
+              height={600}
+            />
           </div>
           <div className={styles.secondItem}>
-            <div style={{fontSize: "40px", fontWeight : "600"}}>Lạc chân vào</div>
-            <div style={{color: COLORS.base, fontSize: "60px", fontWeight : "600"}}>Vườn Hoa Sứ</div>
+            <div style={{ fontSize: "40px", fontWeight: "600" }}>
+              Lạc chân vào
+            </div>
+            <div
+              style={{
+                color: COLORS.base,
+                fontSize: "60px",
+                fontWeight: "600",
+              }}
+            >
+              Vườn Hoa Sứ
+            </div>
           </div>
         </div>
         <div className={styles.gridContainer2}>
           <div>
-            <div className={styles.headingIcon}><FontAwesomeIcon icon={faFire} /></div>
-            <div className={styles.componentTitle}>Đầu truyện mới nhất</div>
+            <div className={styles.headingIcon}>
+              <FontAwesomeIcon icon={faFire} />
+            </div>
+            <div className={styles.componentTitle}>
+              Đầu truyện mới nhất
+            </div>
             <div className={styles.contentBox}>
               <Image src={profilePic2} />
               <div>Chiến binh hoa sứ 2017</div>
-              <div style={{color: COLORS.base}} className={styles.sub}>Lễ Đón Học sinh Mới</div>
+              <div
+                style={{ color: COLORS.base }}
+                className={styles.sub}
+              >
+                Lễ Đón Học sinh Mới
+              </div>
             </div>
           </div>
           <div className={styles.dashLine}></div>
           <div>
-            <div className={styles.headingIcon}><FontAwesomeIcon icon={faStar} /></div>
-            <div className={styles.componentTitle}>Vũ trụ hiện tại</div>
-            <div className={styles.contentBox} >
+            <div className={styles.headingIcon}>
+              <FontAwesomeIcon icon={faStar} />
+            </div>
+            <div className={styles.componentTitle}>
+              Vũ trụ hiện tại
+            </div>
+            <div className={styles.contentBox}>
               <Image src={profilePic2} />
               <div>Lorem ipsum dolor sit amet</div>
-              <div style={{color: COLORS.base}} className={styles.sub}>Lorem ipsum dolor sit amet</div>
+              <div
+                style={{ color: COLORS.base }}
+                className={styles.sub}
+              >
+                Lorem ipsum dolor sit amet
+              </div>
             </div>
           </div>
         </div>
